10d2a5ff6da9748887f6b02d8b316c51c3d4a2e8
Vytenis <vytenis.kondrackis@gmail.com>
2024-07-17 16:15:39 +0300

    implemented kinde auth

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,7 +1,17 @@
 import React from "react";
+import {
+  RegisterLink,
+  LoginLink,
+} from "@kinde-oss/kinde-auth-nextjs/components";
 
 const Login = () => {
-  return <div>Login</div>;
+  return (
+    <div>
+      <LoginLink>Sign in</LoginLink>
+
+      <RegisterLink>Sign up</RegisterLink>
+    </div>
+  );
 };
 
 export default Login;
